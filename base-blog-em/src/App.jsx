a05203bbe04cd302056e8b1d9690a5b66e84cd4d
Esra Kaya <esrakaya165@outlook.com>
2022-11-09 08:59:09 +0000

    initial commit

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,13 +1,22 @@
 import { Posts } from "./Posts";
 import "./App.css";
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {ReactQueryDevtools} from 'react-query/devtools'
+
+// 👇 creating a client which has our cache and all of our default options
+const queryClient = new QueryClient;
 
 function App() {
   return (
     // provide React Query client to App
+    // Everything withing QueryClientProvider can use our React query hooks.
+    <QueryClientProvider client = {queryClient}>
     <div className="App">
       <h1>Blog Posts</h1>
       <Posts />
     </div>
+    <ReactQueryDevtools/>
+    </QueryClientProvider>
   );
 }
 
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
-
+// Were going 👇 to use useQuery when we want to fetch data from out server.
+import {useQuery} from 'react-query'
 import { PostDetail } from "./PostDetail";
+
+// this is because the page limit on the api is 10 (check the https couple of lines down)
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  // for this api the page numberinh starts at  👇
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
-
+  // when the query key (below its in an array) changes, we alert useQuery so that it can refetch the data.
+  // When the currentPage changes, react query will update the data 👇
+  const {data, isError, error, isLoading} = useQuery(['posts', currentPage], () => fetchPosts(currentPage), {staleTime: 2000 });
+  if (isLoading) return <h3 >Loading ...</h3>
+  if (isError) return <> <h3> Bir sorun oldu knk</h3> <p>{error.toString()}</p> </>
   return (
     <>
       <ul>
@@ -31,11 +38,19 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button 
+          disabled = {currentPage <= 1}  
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue -1) 
+            }}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button 
+        disabled = {currentPage >= maxPostPage}
+        onClick={() => {
+            setCurrentPage((previousValue) => previousValue +1)
+        }}>
           Next page
         </button>
       </div>
