5eff3a3c7c7332dfdb22fd9a46d9c64e4cf02ab9
Esra Kaya <esrakaya165@outlook.com>
2022-11-11 08:02:10 +0000

    second project complete

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
 
-import {  QueryClient, QueryClientProvider,} from '@tanstack/react-query'
+import {  QueryClient, QueryClientProvider,} from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient()
@@ -12,8 +12,8 @@ function App() {
     <QueryClientProvider client ={queryClient}>
     <div className="App">
       <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
+      {/* <InfinitePeople /> */}
+      <InfiniteSpecies />
       <ReactQueryDevtools />
     </div>
     </QueryClientProvider>
